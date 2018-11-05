710a9e99f28696d69129722db4d0b5b6eaccd8a7
Ariana Smith-373001575 <373001575@vusdnoc.vusd.k12.ca.us>
2018-11-05 12:39:50 -0800

    Initial Commit

---
+++ src/tempcala/Tempcala.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tempcala;
+
+/**
+ *
+ * @author 373001575
+ */
+public class Tempcala {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
